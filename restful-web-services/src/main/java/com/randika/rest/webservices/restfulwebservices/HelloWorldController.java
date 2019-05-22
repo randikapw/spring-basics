5eef79955d54c28e373f018d9e5a0aade154d7f3
Randika Weerasooriya <randikapw@gmail.com>
2019-05-22 06:51:43 +0530

    Introducing hello world controller for hello world resource.

---
+++ restful-web-services/src/main/java/com/randika/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.randika.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	// GET
+	// URI - /hello-world
+	// method helloWorld
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
